70c3aeacb3d8dc4fe0528b23b0358065bc25a1e7
ysmart <zen.89-89@mail.ru>
2021-07-25 16:13:54 +0300

    USpellBase + наброски по кулдауну

--- Plugins/EffectsSystem/Source/EffectsSystem/Private/SpellBase.cpp
+++ Plugins/EffectsSystem/Source/EffectsSystem/Private/SpellBase.cpp
@@ -3,3 +3,31 @@
 
 #include "SpellBase.h"
 
+void USpellBase::CooldownExpired()
+{
+    OnCooldownExpiredDelegate.Broadcast(this);
+}
+
+bool USpellBase::IsHasCooldown() const
+{
+    UWorld* World = GetWorld();
+    check(World);
+
+    return !World->GetTimerManager().IsTimerPending(Cooldown);
+}
+
+void USpellBase::SetCooldown(float NewCooldown)
+{
+    UWorld* World = GetWorld();
+    check(World);
+
+    World->GetTimerManager().SetTimer(Cooldown, this, &USpellBase::CooldownExpired, NewCooldown);
+}
+
+float USpellBase::GetCooldownTime() const
+{
+    UWorld* World = GetWorld();
+    check(World);
+
+    return World->GetTimerManager().GetTimerRemaining(Cooldown);
+}
--- Plugins/EffectsSystem/Source/EffectsSystem/Public/SpellBase.h
+++ Plugins/EffectsSystem/Source/EffectsSystem/Public/SpellBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "ReplicatedObject.h"
+#include "FloatParameter.h"
 #include "SpellBase.generated.h"
 
 /**
@@ -13,5 +14,29 @@ UCLASS(Abstract, Blueprintable)
 class EFFECTSSYSTEM_API USpellBase : public UReplicatedObject
 {
 	GENERATED_BODY()
+
+    DECLARE_MULTICAST_DELEGATE_OneParam(FOnCooldownExpired, USpellBase const*);
 	
+private:
+
+    UPROPERTY(EditAnywhere)
+    FFloatParameter RecoveryTime;
+
+    FTimerHandle Cooldown;
+
+    FOnCooldownExpired OnCooldownExpiredDelegate;
+
+    virtual void CooldownExpired();
+
+public:
+
+    bool IsHasCooldown() const;
+
+    virtual void  SetCooldown(float NewCooldown);
+    virtual float GetCooldownTime() const;
+
+    FORCEINLINE USpellBase::FOnCooldownExpired& GetOnCooldownExpiredDelegate()
+    {
+        return OnCooldownExpiredDelegate;
+    }
 };
